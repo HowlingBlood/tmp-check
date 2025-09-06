c9ed88446a7207be2ceebf424ce9d6d0712bdc66
HowlingBlood <104342597+HowlingBlood@users.noreply.github.com>
2025-09-06 15:58:08 +0800

    Add TPM Manufacturer and version output

--- TPM.bat
+++ TPM.bat
@@ -3,23 +3,29 @@
 @echo off
 chcp 65001 >nul
 net session >nul 2>&1
-if %errorLevel% neq 0 (
+setlocal enabledelayedexpansion
+
+if !errorLevel! neq 0 (
     echo 未以管理员身份启动，正在请求管理员权限...
     powershell -Command "Start-Process cmd -ArgumentList '/c %~0' -Verb RunAs"
     exit /b
 )
 echo TPM和安全启动检测助手 BY HowlingBlood a.k.a. DELUXO_MKIII
 echo 正在检测...
-set "tpm_status=未开启"
-set "tpm_detail="
-for /f "tokens=*" %%a in ('powershell.exe -command Get-Tpm') do (
-    echo %%a | findstr /i "TpmEnabled" >nul && (
-        echo %%a | findstr /i "True" >nul && (
-            set "tpm_status=开启"
-        )
-    )
+
+for /f "tokens=*" %%a in ('powershell.exe -command "$tpm = Get-Tpm; if($tpm.TpmEnabled){'开启'}else{'未开启'}"') do (
+    set "tpm_status=%%a"
 )
-REM 检查UEFI启动
+
+for /f "tokens=*" %%a in ('powershell.exe -command "$tpm = Get-Tpm; $tpm.ManufacturerIdTxt"') do (
+    set "tpm_manufacturer=%%a"
+)
+
+for /f "tokens=*" %%a in ('powershell.exe -command "$tpm = Get-Tpm; $tpm.ManufacturerVersionFull20"') do (
+    set "tpm_version=%%a"
+)
+
+
 set "uefi_status=未检测"
 for /f "tokens=*" %%a in ('powershell.exe -command "[System.Environment]::GetEnvironmentVariable('firmware_type')"') do (
     echo %%a | findstr /i "UEFI" >nul && (
@@ -30,7 +36,6 @@ if "%uefi_status%"=="未检测" (
     set "uefi_status=Legacy"
 )
 
-REM 检查系统磁盘是否为GPT
 set "gpt_status=未检测"
 for /f "tokens=*" %%a in ('powershell.exe -command "(Get-Disk | Where-Object IsSystem -eq $true).PartitionStyle"') do (
     echo %%a | findstr /i "GPT" >nul && (
@@ -48,8 +53,10 @@ for /f "tokens=*" %%a in ('powershell.exe -command Confirm-SecureBootUEFI') do (
 )
 echo 检测完成。
 
-echo TPM 状态: %tpm_status%
-echo Secure Boot 状态: %secure_boot_status%
+echo TPM状态: !tpm_status!
+echo TPM制造商: !tpm_manufacturer!
+echo 制造商版本: !tpm_version!
+echo 安全启动状态: %secure_boot_status%
 echo UEFI 启动: %uefi_status%
 echo 系统磁盘分区格式: %gpt_status%
 
